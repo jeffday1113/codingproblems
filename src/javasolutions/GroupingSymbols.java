6079a2d909c25ddefe3dd151c3d97dde843718ef
Jeffrey Day <jeffday1113@gmail.com>
2016-05-16 14:36:12 -0400

    Non recursive implementation

--- src/javasolutions/GroupingSymbols.java
+++ src/javasolutions/GroupingSymbols.java
@@ -1,5 +1,7 @@
 package javasolutions;
 
+import java.util.Stack;
+
 public class GroupingSymbols {
     
     /* Determine if grouping symbols is valid
@@ -8,15 +10,43 @@ public class GroupingSymbols {
      * ())(}{ = false */
     
     public static void main(String[] args){
-        
+        GroupingSymbols t = new GroupingSymbols();
+        System.out.println(t.isGroupingValidNonRecursive("({]]})"));
     }
     
-    public boolean isGroupingValid(String str, int startIndex, int endIndex){
-        return false;
+    public boolean isGroupingValidNonRecursive(String str){
+        Stack<Character> stack = new Stack<Character>();
+        for (int i=0; i<str.length(); i++){
+            if (str.charAt(i) == '[' || str.charAt(i) == '{' || str.charAt(i) == '('){
+                stack.push(str.charAt(i));
+            }
+            else if (str.charAt(i) == ']' || str.charAt(i) == '}' || str.charAt(i) == ')'){
+                if (stack.pop() != reverseGrouping(str.charAt(i))){
+                    return false;
+                }
+            }
+        }
+        if (stack.isEmpty())
+            return true;
+        else
+            return false;
     }
     
-    private String reverseGrouping(String grouping){
-        return "";
+    private char reverseGrouping(char grouping){
+        if (grouping == '{')
+            return '}';
+        else if (grouping == '}')
+            return '{';
+        else if (grouping == '(')
+            return ')';
+        else if (grouping == ')')
+            return '(';
+        else if (grouping == '[')
+            return ']';
+        else if (grouping == ']')
+            return '[';
+        else
+            return ' ';
     }
 
 }
